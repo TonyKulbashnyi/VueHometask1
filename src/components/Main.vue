46508970fe40b707cb28c1b14e57d2644fb3293c
Anton Kulbashnyi <kulbashnyi.a@gmail.com>
2021-06-01 19:29:34 +0300

    hometask 1

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,84 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        {{ hello }}
+    <div  v-show="!result">
+    <v-row
+      class="d-flex justify-center">
+      <v-col cols="4">
+        <form action="#">
+          <v-text-field
+            label="email"
+            v-model="formData.email"
+          />
+          <v-text-field
+            label="name"
+            v-model="formData.name"
+          />
+          <v-text-field
+            label="surname"
+            v-model="formData.surname"
+          />
+          <v-text-field
+            label="phone"
+            v-model="formData.phone"
+          />
+          <v-divider />
+          <v-btn
+            class="primary my-3"
+            @click="addGuest"
+          >
+            addGuest
+          </v-btn>
+           <v-text-field
+            v-for="(guest, index) in formData.guests"
+            :key="index"
+            label="guest"
+            v-model="formData.guests[index]"
+            @dblclick="removeGuest(index)"
+          />
+        </form>
+        <v-btn
+            align="center"
+            class="secondary my-3"
+            @click="result = !result"
+          >
+            showResults
+          </v-btn>
       </v-col>
     </v-row>
+    </div>
+    <div v-show="result">
+      <v-row
+        class="d-flex justify-center"
+      >
+        <v-col cols="6">
+          <ol>
+            <li>
+              email: {{formData.email}}
+            </li>
+            <li>
+              name: {{formData.name}}
+            </li>
+            <li>
+              surname: {{formData.surname}}
+            </li>
+            <li>
+              phone: {{formData.phone}}
+            </li>
+            <li>
+              guests:
+                <ol>
+                  <li
+                    v-for="(guest, index) in formData.guests"
+                    :key="index"
+                  >
+                    {{guest}}
+                  </li>
+                </ol>
+            </li>
+          </ol>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
@@ -13,7 +87,24 @@
     name: 'Main',
 
     data: () => ({
-      hello: 'Hello 🌍 '
+      formData: {
+        email: '',
+        name: '',
+        surname: '',
+        phone: '',
+        guests: [
+          ''
+        ]
+      },
+      result: false
     }),
+    methods: {
+      addGuest: function () {
+        this.formData.guests.push('')
+      },
+      removeGuest: function (index) {
+        this.formData.guests.splice(index, 1)
+      }
+    }
   }
 </script>
